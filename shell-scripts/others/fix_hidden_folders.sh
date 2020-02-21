8d551b43aceaf75db68e204d117de321b6e75d23
Douglas Medeiros <medeirosinacio@outlook.com>
2020-02-21 15:16:23 -0300

    refactor

--- shell-scripts/others/fix_hidden_folders.sh
+++ shell-scripts/others/fix_hidden_folders.sh
@@ -4,22 +4,20 @@
 # Autor: Douglas Medeiros <medeirosinacio@outlook.com>
 
 # pastas a serem ocultadas
-declare -a folders=('.vagrant' '.git' '.idea')
-
-# Entra na partição
-cd d:
+declare -a folder=("/d/Code")
+declare -a folders_hide=('.vagrant' '.git' '.idea')
 
 # Faz a checagem se a pasta Code existe
-if [ -d Code ]; then
+if [ -d "$folder" ]; then
 
   # Resgata as pastas e subpastas do Code com no maximo 10 subpastas
-  find Code -maxdepth 10 -type d -print0 |
+  find "$folder" -maxdepth 10 -type d -print0 |
 
     # Faz loop nas pastas resgatadas
     while IFS= read -rd '' dir; do
 
       # Loop no array com os diretorios a ocultar
-      for i in "${folders[@]}"; do
+      for i in "${folders_hide[@]}"; do
 
         # Caso o diretorio atual seja de mesmo nome do diretorio para ocultar
         if [[ "$dir" == *"$i" ]]; then
