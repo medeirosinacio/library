18f3e2e7fefe8fe6eeed568fa7afa281f323378e
Douglas Medeiros <41749399+medeirosinacio@users.noreply.github.com>
2020-05-15 01:18:46 -0300

    Create amaterasu.sh

---
+++ shell-scripts/docker/amaterasu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Stop all containers
+docker stop `docker ps -qa`
+
+# Remove all containers
+docker rm `docker ps -qa`
+
+# Remove all images
+docker rmi -f `docker images -qa `
+
+# Remove all volumes
+docker volume rm $(docker volume ls -q --filter dangling=true)
+
+# Remove all networks
+docker network rm `docker network ls -q`
