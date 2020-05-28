8c8c4f083cff841a18eca24fd5799013050bb3ed
Douglas Medeiros <41749399+medeirosinacio@users.noreply.github.com>
2020-05-28 11:06:54 -0300

    Update gitlab-runner-centos8.sh

--- shell-script/install/gitlab-runner-centos8.sh
+++ shell-script/install/gitlab-runner-centos8.sh
@@ -13,10 +13,12 @@ if ! id -u "deployer" > /dev/null 2>&1; then
     exit 1
 fi
 
-curl -L --output /bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
 
-chmod +x /bin/gitlab-runner
+chmod +x /usr/local/bin/gitlab-runner
 
 gitlab-runner install --user=deployer --working-directory=/home/deployer
+
+ln -s /usr/local/bin/gitlab-runner /bin/gitlab-runner
 
 gitlab-runner start
