8bd77527af2d7465b667348a026af490dfc0d8de
Douglas Medeiros <41749399+medeirosinacio@users.noreply.github.com>
2020-05-28 02:07:14 -0300

    Create gitlab-runner-centos8.sh

---
+++ shell-script/install/gitlab-runner-centos8.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# Check if user exists
+if ! id -u "deployer" > /dev/null 2>&1; then
+    echo "The user does not exist; execute below commands to crate and try again:"
+    echo "  root@sh1:~# useradd --comment 'deployer' --create-home deployer -p ************* --shell /bin/bash"
+    exit 1
+fi
+
+curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+
+chmod +x /bin/gitlab-runner
+
+gitlab-runner install --user=deployer --working-directory=/home/deployer
+
+gitlab-runner start
