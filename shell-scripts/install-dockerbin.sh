c201dc7133a00c8279416fefcab64fd1ac0f84bb
medeirosinacio <medeirosiancio@outlook.com>
2021-01-15 13:20:40 -0300

    fix dos2unix native  bin

--- shell-scripts/install-dockerbin.sh
+++ shell-scripts/install-dockerbin.sh
@@ -3,7 +3,8 @@
 sudo rm -rf /tmp/library
 git clone https://github.com/medeirosinacio/library.git /tmp/library
 
-dos2unix /tmp/library/docker-bin-container/*
+sed -i -e 's/^M$//' /tmp/library/docker-bin-container/*
+sed -i -e 's/\r$//' /tmp/library/docker-bin-container/*
 
 chmod +x /tmp/library/docker-bin-container/*
 chmod 777 /tmp/library/docker-bin-container/*
