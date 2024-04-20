5c8ae9c237c0c269273a7244353c1a58384ecb24
Douglas Medeiros <eu@douglasmedeiros.dev>
2024-04-19 21:21:35 -0300

    feat: install ctop

--- shell-scripts/install/docker_ubuntu.sh
+++ shell-scripts/install/docker_ubuntu.sh
@@ -36,3 +36,6 @@ if [[ ! -z $wsl ]]; then
     sudo chmod 644 /etc/default/docker
   fi
 fi
+
+sudo wget https://github.com/bcicen/ctop/releases/download/v0.7.7/ctop-0.7.7-linux-amd64 -O /usr/local/bin/ctop
+sudo chmod +x /usr/local/bin/ctop
